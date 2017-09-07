dfab6b571f2dcc9eae38512eda00447a06b878f3
MJ Rossetti <datacreativellc@gmail.com>
2017-09-07 18:51:48 -0400

    Add different physics to the moving character

--- ppb_game.py
+++ ppb_game.py
@@ -12,9 +12,18 @@ class Player(DirtySprite):
         self.rect = self.image.get_rect()
         self.scene = scene
 
-    def update(self, time_delta):
-        self.rect.center = mouse.get_pos()
-        self.dirty = True
+    #def update(self, time_delta):
+    #    self.rect.center = mouse.get_pos()
+    #    self.dirty = True
+
+    def update(self, *args):
+        mouse_x, mouse_y = mouse.get_pos()
+        diff_x = max(min(mouse_x - self.rect.centerx, 5), -5)
+        diff_y = max(min(mouse_y - self.rect.centery, 5), -5)
+        self.rect.centerx += diff_x
+        self.rect.centery += diff_y
+        if diff_x or diff_y:
+            self.dirty = True
 
 class Game(BaseScene):
 
