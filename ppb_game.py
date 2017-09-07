45db594808df2a37a8709b3e798bb4aae80e12aa
MJ Rossetti <datacreativellc@gmail.com>
2017-09-07 19:34:28 -0400

    Remove artificial slowness of character movement

--- ppb_game.py
+++ ppb_game.py
@@ -48,14 +48,24 @@ class Player(DirtySprite):
     #    self.rect.center = mouse.get_pos()
     #    self.dirty = True
 
+    #def update(self, time_delta):
+    #    mouse_x, mouse_y = mouse.get_pos()
+    #    diff_x = max(min(mouse_x - self.rect.centerx, 5), -5)
+    #    diff_y = max(min(mouse_y - self.rect.centery, 5), -5)
+    #    self.rect.centerx += diff_x
+    #    self.rect.centery += diff_y
+    #    if diff_x or diff_y:
+    #        self.dirty = True
+#
+    #    pressed = mouse.get_pressed()
+    #    if pressed[0] and self.bullet_delay > self.bullet_limiter:
+    #        Bullet(self.scene, self.rect.midtop)
+    #        self.bullet_delay = 0
+    #    self.bullet_delay += time_delta
+
     def update(self, time_delta):
-        mouse_x, mouse_y = mouse.get_pos()
-        diff_x = max(min(mouse_x - self.rect.centerx, 5), -5)
-        diff_y = max(min(mouse_y - self.rect.centery, 5), -5)
-        self.rect.centerx += diff_x
-        self.rect.centery += diff_y
-        if diff_x or diff_y:
-            self.dirty = True
+        self.rect.center = mouse.get_pos()
+        self.dirty = True
 
         pressed = mouse.get_pressed()
         if pressed[0] and self.bullet_delay > self.bullet_limiter:
